5955de0ad7b026ec6e4175029fac36af825cb340
meateviler <meateviler@msn.com>
2017-07-01 11:40:12 +0800

    create PageContext

--- GOOS_SampleTests/PageObjects/BudgetCreatePage.cs
+++ GOOS_SampleTests/PageObjects/BudgetCreatePage.cs
@@ -7,26 +7,29 @@ namespace GOOS_SampleTests.steps
 		public BudgetCreatePage(FluentTest test)
 			: base(test)
 		{
+			this.Url = "{PageContext.Domain}/budget/add";
 		}
 
 		public BudgetCreatePage Amount(int amount)
 		{
-			throw new System.NotImplementedException();
+			I.Enter(amount.ToString()).In("#amount");
+			return this;
 		}
 
 		public BudgetCreatePage Month(string yearMonth)
 		{
-			throw new System.NotImplementedException();
+			I.Enter(yearMonth).In("#month");
+			return this;
 		}
 
-		public BudgetCreatePage AddBudget()
+		public void AddBudget()
 		{
-			throw new System.NotImplementedException();
+			I.Click("input[type=\"submit\"]");
 		}
 
-		public BudgetCreatePage ShouldDisplay(string message)
+		public void ShouldDisplay(string message)
 		{
-			throw new System.NotImplementedException();
+			I.Assert.Text(message).In("#message");
 		}
 	}
 }
---
+++ GOOS_SampleTests/PageObjects/PageContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GOOS_SampleTests.PageObjects
+{
+	internal class PageContext
+	{
+		public static string Domain
+		{
+			get { return "http://localhost:58527"; }
+		}
+	}
+}
